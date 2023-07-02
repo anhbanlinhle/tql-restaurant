42c908a901bca2ce534ddbe2456f786349066a81
anhbanlinhle <vietlinhleviet@gmail.com>
2023-07-03 00:16:22 +0700

    done oauth

--- server/src/model/verifyOAuth.js
+++ server/src/model/verifyOAuth.js
@@ -1,5 +1,5 @@
 import { OAuth2Client } from 'google-auth-library'
-
+require("dotenv").config()
 const client = new OAuth2Client(
   {
     clientId: process.env.GOOGLE_CLIENT_ID,
@@ -10,12 +10,16 @@ const client = new OAuth2Client(
 
 // Call this function to validate OAuth2 authorization code sent from client-side
 let verifyCode = async (code) => {
-  let { tokens } = await client.getToken(code)
-  client.setCredentials({ access_token: tokens.access_token })
-  const userInfo = await client.request({
-    url: 'https://www.googleapis.com/oauth2/v3/userinfo'
-  })
-  return userInfo.data
+  try {
+    let { tokens } = await client.getToken(code)
+    client.setCredentials({ access_token: tokens.access_token })
+    const userInfo = await client.request({
+      url: 'https://www.googleapis.com/oauth2/v3/userinfo'
+    })
+    return userInfo.data
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 module.exports = {
