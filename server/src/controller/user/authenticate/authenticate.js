782efed737e84d784f1e8f950b5d6ca28647eb6f
anhbanlinhle <vietlinhleviet@gmail.com>
2023-07-03 07:48:19 +0700

    draft authentication using oauth

--- server/src/controller/user/authenticate/authenticate.js
+++ server/src/controller/user/authenticate/authenticate.js
@@ -3,10 +3,24 @@ import { verifyCode } from '../../../model/verifyOAuth';
 
 
 let authenticate = async (req, res) => {
-  console.log(req.body.code)
-  let userInfo = await verifyCode(req.body.code)
-  console.log(userInfo)
-  return res.send('ok')
+  try {
+    let userInfo = await verifyCode(req.body.code)
+    let query = `select * from available_mail where email = ?`
+    let result = await pool.execute(query, [userInfo.email])
+    if (result[0].length === 0) {;
+      return res.status(400).send('Email has not been registered')
+    }
+    else {
+      let query = `select * from user where email = ?`
+      let result = await pool.execute(query, [userInfo.email])
+      req.session.userid = result[0][0].id
+      req.session.token = userInfo.token
+      return res.send(userInfo)
+    }
+  }
+  catch (err) {
+    console.log(err)
+  }
 }
 
 module.exports = {
