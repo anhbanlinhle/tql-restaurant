c8b026d462d1611c92d89ad3191b2c2e5cf914d9
vmtuan12 <21020664@vnu.edu.vn>
2023-07-03 22:11:15 +0700

    insert new user after logging in into user table if user hasnt been created

--- server/src/controller/user/authenticate/authenticate.js
+++ server/src/controller/user/authenticate/authenticate.js
@@ -8,25 +8,41 @@ let authenticate = async (req, res) => {
     let userInfo = await verifyCode(req.body.code)
     let query = `select * from available_mail where email = ?`
     let result = await pool.execute(query, [userInfo.email])
-    if (result[0].length === 0) {;
+    if (result[0].length === 0) {
       return res.status(400).send({ message: 'Email has not been registered' })
     }
     else {
       let query = `select id, name, type from user where email = ?`
       let result = await pool.execute(query, [userInfo.email])
-      await pool.execute(`update user set name = ? where email = ?`, [userInfo.name, userInfo.email])
-      req.session.userid = result[0][0].id
-      req.session.token = userInfo.token
-      req.session.email = userInfo.email
+      console.log(result[0], userInfo);
+      let jwtPayload;
+      if (result[0].length === 0) {
+        const queryToCreateNewUser = 
+            `INSERT INTO user (email, name, avatar, debt, type, phone, ssn) VALUES (?, ?, ?, 0, 0, '123456789', '123456789')`
+        await pool.execute(queryToCreateNewUser, [userInfo.email, userInfo.name, userInfo.picture])
+        const getIdAndType = await pool.execute(`select id from user where email = ?`, [userInfo.email])
+        req.session.userid = getIdAndType[0][0].id
+        req.session.email = userInfo.email
+        jwtPayload = {
+          email: req.session.email,
+          id: req.session.userid,
+          name: userInfo.name,
+          type: getIdAndType.type
+        }
+      } else {
+        await pool.execute(`update user set name = ? where email = ?`, [userInfo.name, userInfo.email])
+        req.session.userid = result[0][0].id
+        req.session.token = userInfo.token
+        req.session.email = userInfo.email
 
-      const payload = {
-        email: req.session.email,
-        id: req.session.userid,
-        name: result[0][0].name,
-        type: result[0][0].type
+        jwtPayload = {
+          email: req.session.email,
+          id: req.session.userid,
+          name: result[0][0].name,
+          type: result[0][0].type
+        }
       }
-      
-      const authToken = jwt.sign(payload, process.env.SECRET, {expiresIn: '20000000000000s'})
+      const authToken = jwt.sign(jwtPayload, process.env.SECRET, {expiresIn: '20000000000000s'})
       req.session.token = authToken
       
       return res.send({ token: authToken })
