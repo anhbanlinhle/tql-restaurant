431a948f1b43e3e84987d57086d0e9796d52d0a3
vmtuan12 <21020664@vnu.edu.vn>
2023-07-08 23:02:01 +0700

    change method in server side

--- server/src/route/web.js
+++ server/src/route/web.js
@@ -8,7 +8,7 @@ const initWebRoute = (app) => {
   router.get('/', homeController.homepage)
   router.post('/login', homeController.authenticate)
 
-  router.post('/dishes', verifyToken, homeController.getAvailableDishes)
+  router.get('/dishes', verifyToken, homeController.getAvailableDishes)
   router.get('/dish/:id', verifyToken, homeController.getSpecifiedDish)
 
   return app.use('/', router)
