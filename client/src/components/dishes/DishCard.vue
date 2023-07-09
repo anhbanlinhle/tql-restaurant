3ac5666adf819254471542467b3d4740da15bc59
vmtuan12 <21020664@vnu.edu.vn>
2023-07-09 12:16:04 +0700

    effect for dish card component

--- client/src/components/dishes/DishCard.vue
+++ client/src/components/dishes/DishCard.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 
 const props = defineProps(['imgSrc', 'dishName', 'dishType', 'id']);
 const emits = defineEmits(['openModal']);
@@ -11,6 +11,14 @@ const colorOption = {
     red: 'text-[#f34971] font-bold',
     celery: 'text-[#aabb5d] font-bold',
 };
+
+const colorHover = {
+    primary: 'bg-[#52a1f5] text-white',
+    success: 'bg-[#39c0c8] text-white',
+    neon: 'bg-[#6c47ff] text-white',
+    red: 'bg-[#f34971] text-white',
+    celery: 'bg-[#aabb5d] text-white',
+}
 
 const selectedColor = computed(() => {
     if (props.dishType === 'Appetizer') {
@@ -26,6 +34,22 @@ const selectedColor = computed(() => {
     }
 });
 
+const selectedColorHover = computed(() => {
+    if (props.dishType === 'Appetizer') {
+        return colorHover.success
+    } else if (props.dishType === 'Main dish') {
+        return colorHover.primary
+    } else if (props.dishType === 'Side dish') {
+        return colorHover.neon
+    } else if (props.dishType === 'Dessert') {
+        return colorHover.red
+    } else {
+        return colorHover.celery
+    }
+})
+
+const mouseOverInfoIcon = ref(false);
+
 </script>
 
 <template>
@@ -40,7 +64,10 @@ const selectedColor = computed(() => {
                     @click="$emit('openModal', id)"
                     xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                     stroke-width="1.5" stroke="currentColor"
-                    class="w-6 h-6 text-gray-500 cursor-pointer"
+                    class="w-6 h-6 cursor-pointer rounded-full transition-colors duration-300 ease-in-out"
+                    @mouseenter="mouseOverInfoIcon = true"
+                    @mouseleave="mouseOverInfoIcon = false"
+                    :class="mouseOverInfoIcon ? selectedColorHover : selectedColor"
                 >
                     <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
                 </svg>
